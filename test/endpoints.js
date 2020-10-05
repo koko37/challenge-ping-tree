5dc878a9687bfcab8a797fec2d9cd0ea8d53333d
David Guttman <david@js.la>
2020-10-05 13:55:51 -0700

    Initial commit

---
+++ test/endpoints.js
@@ -0,0 +1,17 @@
+process.env.NODE_ENV = 'test'
+
+var test = require('ava')
+var servertest = require('servertest')
+
+var server = require('../lib/server')
+
+test.serial.cb('healthcheck', function (t) {
+  var url = '/health'
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.is(res.body.status, 'OK', 'status is ok')
+    t.end()
+  })
+})
